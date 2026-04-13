afd5d208ade045ee3dfbd01a87b60d23384aa060
pk-co <prasacode@email.com>
2026-04-14 00:16:48 +0530

    [UPD] price

--- line_color/__manifest__.py
+++ line_color/__manifest__.py
@@ -16,7 +16,7 @@
     "maintainer": "optindev",
     "website": "https://www.optinsolutions.com",
     "support": "optinassist@gmail.com",
-    "price": 0.5,
+    "price": 9.5,
     "currency": "USD",
     "license": "LGPL-3",
     "depends": ["base"],
